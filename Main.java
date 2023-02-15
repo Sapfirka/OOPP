819116d333f54ff847610b68fca03fb752a5d41a
Sapfirka <sapfirka@mail.ru>
2023-02-16 00:47:08 +0300

    First commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 7;
+        post.birthday.year = 1991;
+        post.name = "Иван";
+        post.passport = "444 №444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+
+    }
+}
---
+++ Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public FormDate birthday = new FormDate();
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
+
